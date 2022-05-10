5405094798a19ad63ea1653135fdacced69d84c3
cyberspace-power <gjs3qd@virginia.edu>
2022-05-09 23:32:14 -0400

    Added category 17

--- create_text_dataset.py
+++ create_text_dataset.py
@@ -6,7 +6,7 @@ import os
 import random
 
 # How large of a sample of the data to take
-samples = 4723  # This is how many we have from category 1 (fewest of any category)
+samples = 4940  # This is how many we have from category 1 (fewest of any category)
 
 # Note: Assumes that original data files are located in the following directories:
 #   ../Data/category_wise_data
@@ -31,8 +31,9 @@ else:
 # that will allow us to say Yes ('y') or No ('n') if we want to accept a data point. See the manual_select def below.
 image_files = os.listdir('../Data/category_wise_thumbnails_original/' + str(category_num))
 random.shuffle(image_files)
-flag = True if samples - new_text_df.shape[0] <= 10 else False  # Decides whether to manually select data or not
+flag = True if samples - new_text_df.shape[0] <= 50 else False  # Decides whether to manually select data or not
 
+count = 1
 for file in image_files:  # Iterate through shuffled file names
     if new_text_df.shape[0] == samples:  # Number of wanted samples achieved
         break
@@ -41,10 +42,11 @@ for file in image_files:  # Iterate through shuffled file names
     if file[:-4] in set(full_text_df['Video Id']) and file[:-4] not in set(new_text_df["Video Id"]):
         i = full_text_df.index[full_text_df['Video Id'] == file[:-4]].tolist()[0]  # Get index of row with corresponding video id
         if flag:
-            choice = input("Do you want to add the following data point:\n\tVideo Id:\t" + file[:-4] +
+            choice = input(str(count) + ": Do you want to add the following data point:\n\tVideo Id:\t" + file[:-4] +
                            "\n\tTitle:\t" + str(full_text_df.at[i, 'Title']) + "\n\tDesc:\t" +
                            str(full_text_df.at[i, 'Description']) + "\n\nType 'y' for YES or 'n' for NO: ")
             if choice == 'y':
+                count += 1
                 new_text_df.loc[len(new_text_df)] = [file[:-4], full_text_df.at[i, 'Title'], full_text_df.at[i, 'Description']]
         else:
             new_text_df.loc[len(new_text_df)] = [file[:-4], full_text_df.at[i, 'Title'], full_text_df.at[i, 'Description']]
--- get_image_dataset.py
+++ get_image_dataset.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 import pandas as pd
-import tqdm
 
 category_num = input("Please input the category number to collect the images for: ")
 dataset = pd.read_csv("./dataset/category_wise_data/category_" + str(category_num) + ".csv")
